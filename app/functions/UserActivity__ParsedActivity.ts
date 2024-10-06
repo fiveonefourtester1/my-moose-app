265e1602a6321746f0f262432a1a381d6eeeb617
514tester1 <514.tester.1@gmail.com>
2024-10-06 11:50:41 -0600

    update to latest

--- app/functions/UserActivity__ParsedActivity.ts
+++ app/functions/UserActivity__ParsedActivity.ts
@@ -1,11 +1,10 @@
-
-// Example flow function: Converts local timestamps in UserActivity data to UTC.
+// Example streaming function: Converts local timestamps in UserActivity data to UTC.
 
 // Imports: Source (UserActivity) and Destination (ParsedActivity) data models.
-import { ParsedActivity, UserActivity } from "../../../datamodels/models";
+import { ParsedActivity, UserActivity } from "datamodels/models";
 
 // The 'run' function transforms UserActivity data to ParsedActivity format.
-// For more details on how Moose flows work, see: https://docs.moosejs.com
+// For more details on how Moose streaming functions work, see: https://docs.moosejs.com
 export default function run(source: UserActivity): ParsedActivity {
   // Convert local timestamp to UTC and return new ParsedActivity object.
   return {
