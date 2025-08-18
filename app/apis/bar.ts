794038f0891802db4d6e8e6d98e625713ec8965d
514tester1 <514.tester.1@gmail.com>
2025-08-18 14:53:50 -0600

    update to latest template

--- app/apis/bar.ts
+++ app/apis/bar.ts
@@ -1,45 +1,55 @@
-import {
-  createConsumptionApi,
-  ConsumptionHelpers as CH,
-} from "@514labs/moose-lib";
+import { ConsumptionApi, MooseCache } from "@514labs/moose-lib";
+import { BarAggregatedMV } from "../views/barAggregated";
 import { tags } from "typia";
-
-// curl "http://localhost:4000/consumption/bar?orderBy=totalRows"
 
 // This file is where you can define your APIs to consume your data
 interface QueryParams {
-  orderBy: "totalRows" | "rowsWithText" | "maxTextLength" | "totalTextLength";
+  orderBy?: "totalRows" | "rowsWithText" | "maxTextLength" | "totalTextLength";
   limit?: number;
-  startDay?: number & tags.Type<"int32"> & tags.Minimum<1> & tags.Maximum<31>;
-  endDay?: number & tags.Type<"int32"> & tags.Minimum<1> & tags.Maximum<31>;
+  startDay?: number & tags.Type<"int32">;
+  endDay?: number & tags.Type<"int32">;
 }
 
-// createConsumptionApi uses compile time code generation to generate a parser for QueryParams
-export default createConsumptionApi<QueryParams>(
+interface ResponseData {
+  dayOfMonth: number;
+  totalRows?: number;
+  rowsWithText?: number;
+  maxTextLength?: number;
+  totalTextLength?: number;
+}
+
+export const BarApi = new ConsumptionApi<QueryParams, ResponseData[]>(
+  "bar",
   async (
     { orderBy = "totalRows", limit = 5, startDay = 1, endDay = 31 },
-    { client, sql }
+    { client, sql },
   ) => {
+    const cache = await MooseCache.get();
+    const cacheKey = `bar:${orderBy}:${limit}:${startDay}:${endDay}`;
+
+    // Try to get from cache first
+    const cachedData = await cache.get<ResponseData[]>(cacheKey);
+    if (cachedData && Array.isArray(cachedData) && cachedData.length > 0) {
+      return cachedData;
+    }
+
     const query = sql`
-      SELECT 
-        dayOfMonth,
-        ${CH.column(orderBy)}
-      FROM BarAggregated_MV
-      WHERE 
-        dayOfMonth >= ${startDay} 
-        AND dayOfMonth <= ${endDay}
-      ORDER BY ${CH.column(orderBy)} DESC
-      LIMIT ${limit}
-    `;
+        SELECT 
+          ${BarAggregatedMV.targetTable.columns.dayOfMonth},
+          ${BarAggregatedMV.targetTable.columns[orderBy]}
+        FROM ${BarAggregatedMV.targetTable}
+        WHERE 
+          dayOfMonth >= ${startDay} 
+          AND dayOfMonth <= ${endDay}
+        ORDER BY ${BarAggregatedMV.targetTable.columns[orderBy]} DESC
+        LIMIT ${limit}
+      `;
 
-    const data = await client.query.execute<{
-      dayOfMonth: number;
-      totalRows?: number;
-      rowsWithText?: number;
-      maxTextLength?: number;
-      totalTextLength?: number;
-    }>(query);
+    const data = await client.query.execute<ResponseData>(query);
+    const result: ResponseData[] = await data.json();
 
-    return data;
-  }
+    await cache.set(cacheKey, result, 3600); // Cache for 1 hour
+
+    return result;
+  },
 );
